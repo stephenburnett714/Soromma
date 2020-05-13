4338000a28805cdeba3e161efbe4bfecafd7066f
Stephen Burnett <stephenburnett@Stephens-MBP.fios-router.home>
2020-05-12 23:55:08 -0400

    added logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import About from './Components/About'
+import Cotact from './Components/Contact'
+import Home from './Components/Home'
+import Navbar from './Components/Navbar'
+import Portfolio from './Components/Portfolio'
+import Team from './Components/Team'
+import Testimonials from './Components/Testimonials'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Navbar />
+      <Home />
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import sorroma from '../Images/Soromma.png'
 
 export default function Navbar() {
     return(
         <div>
-            
+            <img src={sorroma} alt="logo"/>
         </div>
     )
 }
