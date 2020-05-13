b4f15238d383845b5c2eba8495d75a5a117e5a3f
Stephen Burnett <stephenburnett@Stephens-MBP.fios-router.home>
2020-05-13 15:41:07 -0400

    first draft of navbar complete

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import background from '../Images/home-background.jpg'
 
 export default function Home() {
     return(
         <div className="home" id="home">
-<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, voluptatum animi tempore, quod explicabo magni modi sapiente debitis aperiam aut velit nulla nesciunt fugit sit quia repellat assumenda ut numquam?Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, eaque sapiente iusto accusantium nostrum possimus a. Quaerat distinctio ratione accusantium autem consequatur sequi porro odit eligendi! Ut deserunt cumque animi.</div>
+            <div className="home-background" src={background} ></div>
         </div>
     )
 }
