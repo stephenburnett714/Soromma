0d194fe0b248458a80c7360a91f354503733185a
Stephen Burnett <stephenburnett@Stephens-MBP.fios-router.home>
2020-05-13 01:06:10 -0400

    logo and nav links in navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import About from './Components/About'
-import Cotact from './Components/Contact'
+import Contact from './Components/Contact'
 import Home from './Components/Home'
 import Navbar from './Components/Navbar'
 import Portfolio from './Components/Portfolio'
+import Services from './Components/Services'
 import Team from './Components/Team'
 import Testimonials from './Components/Testimonials'
 import './App.css';
@@ -13,6 +14,12 @@ function App() {
     <div className="App">
       <Navbar />
       <Home />
+      <About />
+      <Services />
+      <Testimonials />
+      <Portfolio />
+      <Team />
+      <Contact />
     </div>
   );
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import sorroma from '../Images/Soromma.png'
 
 export default function Navbar() {
-    return(
-        <div>
-            <img src={sorroma} alt="logo"/>
+
+    return (
+        <div className="">
+
+            <div className="nav-bar-logo-container">
+                <img className="nav-bar-logo" src={sorroma} alt="logo" />
+            </div>
+
+            <div className="nav-title-container">
+                <div className="nav-title">Home</div>
+                <div className="nav-title">About</div>
+                <div className="nav-title">Services</div>
+                <div className="nav-title">Testimonials</div>
+                <div className="nav-title">Portfolio</div>
+                <div className="nav-title">Team</div>
+                <div className="nav-title">Contact</div>
+            </div>
+
         </div>
     )
 }
