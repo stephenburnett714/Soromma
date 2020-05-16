fb2b93f3600f17567c19ab6073d1752261c00a10
Stephen Burnett <stephenburnett@Stephens-MBP.fios-router.home>
2020-05-16 18:55:05 -0400

    fully responsive

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,19 +6,24 @@ import { faCompass,faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons
 export default function Contact() {
     return(
         <div className="contact" id="contact">
+
+        
             <div className="header-title">Contact Us</div>
+                        <div className="contact-header">Office Hours</div>
+            <div className="days-open">Monday through Friday</div>
+            <div className="Office-time">9:00am - 5:00pm</div>
             <div className="contact-card-container">
                 <a rel="noopener noreferrer" target="_blank" href="http://maps.google.com/?q=Empire State Building, New York, NY 10001">
                 <div className="contact-card">
                     <FontAwesomeIcon icon={faCompass} size= '2x'/>
-                    <div>Our Address</div>
+                    <div className="contact-header">Our Address</div>
                     <div>Empire State Building, New York, NY 10001</div>
                 </div>
                 </a>
                 <a href="tel:[13472928240]">
                 <div className="contact-card">
                     <FontAwesomeIcon icon={faPhone} size= '2x'/>
-                    <div>Call Us</div>
+                    <div className="contact-header">Call Us</div>
                     <div>(347) 292-8240</div>
                 </div>
                 </a>
@@ -26,14 +31,11 @@ export default function Contact() {
                 <a href="mailto:contact@sorommatechnologies.com">
                 <div className="contact-card">
                     <FontAwesomeIcon icon={faEnvelope} size= '2x'/>
-                    <div>Email Us</div>
+                    <div className="contact-header">Email Us</div>
                     <div>contact@sorommatechnologies.com</div>
                 </div>
                 </a>
             </div>
-            <div>Office Hours</div>
-            <div>Monday through Friday</div>
-            <div>9am - 5pm</div>
 
         </div>
     )
